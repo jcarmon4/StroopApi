ddc20dc9247f449b4f721290fe5d829fdfba2968
Alejocram <jcarmon4@eafit.edu.co>
2019-08-25 12:59:30 -0500

    Setting to use SQL DB Express String Connection and

---
+++ StroopApi/Migrations/20190825172306_Initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StroopApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RecordActivities",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    IdUser = table.Column<long>(nullable: false),
+                    Stage = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true),
+                    Text = table.Column<string>(nullable: true),
+                    Ink = table.Column<string>(nullable: true),
+                    Time = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecordActivities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RecordActivities");
+        }
+    }
+}
--- StroopApi/Startup.cs
+++ StroopApi/Startup.cs
@@ -27,9 +27,12 @@ namespace StroopApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<StroopApiContext>(opt =>
-                opt.UseInMemoryDatabase("StroopApiList"));
+            //services.AddDbContext<StroopApiContext>(opt =>
+            //    opt.UseInMemoryDatabase("StroopApiList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<StroopApiContext>(options =>
+              options.UseSqlServer(
+                  Configuration.GetConnectionString("StroopApiContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
